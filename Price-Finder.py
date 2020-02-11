215cfa393202e9845e2c6319100a37e7f01d6a04
Lucas Oliveira <lookus.so@gmail.com>
2020-02-10 19:49:08 -0500

    Created some files

---
+++ Price-Finder.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+#initializing tkitner
+root = Tk()
+
+#title
+root.title("Price Finder")
+
+#window size
+root.geometry("800x700")
+
+#Label
+L1 = Label(root, text = "Item", font = ("bold", 18), padx = 20, pady = 20)
+E1 = Entry(root, width = 20, bd = 2)
+B1 = Button(root, text = "Enter", font = ("bold", 14), height = 1, width = 4, command = search)
+#grid
+L1.grid(row = 0, column = 0)
+E1.grid(row = 0, column = 1)
+B1.grid(row = 0, column = 2)
+#Search command
+def search():
+    pass
+#List box
+Lb1 = Listbox(root, bd = 0, width = 40)
+#grid
+Lb1.grid(row = 3, column = 1)
+#main loop that runs the game
+root.mainloop()
