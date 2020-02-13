bb376281e8152eb88ca170138379920e67656c02
Lucas Oliveira <lookus.so@gmail.com>
2020-02-13 17:22:27 -0500

    Did some tests with Beautiful Soup

--- Price-Finder.py
+++ Price-Finder.py
@@ -1,3 +1,4 @@
+from soupz import result
 from tkinter import *
 
 #initializing tkitner
@@ -9,17 +10,19 @@ root.title("Price Finder")
 #window size
 root.geometry("800x700")
 
+#Search command
+def search():
+    pass
+
 #Label
 L1 = Label(root, text = "Item", font = ("bold", 18), padx = 20, pady = 20)
 E1 = Entry(root, width = 20, bd = 2)
-B1 = Button(root, text = "Enter", font = ("bold", 14), height = 1, width = 4, command = search)
+B1 = Button(root, text = "Enter", font = ("bold", 14), height = 1, width = 4, command = result)
+
 #grid
 L1.grid(row = 0, column = 0)
 E1.grid(row = 0, column = 1)
 B1.grid(row = 0, column = 2)
-#Search command
-def search():
-    pass
 #List box
 Lb1 = Listbox(root, bd = 0, width = 40)
 #grid
---
+++ soupz.py
@@ -0,0 +1,11 @@
+import bs4 as bs
+import urllib.request
+#This is the source URL that we are going to be extracting data from
+source = urllib.request.urlopen('http://127.0.0.1:5500/test.html').read()
+
+#This is the soup. The soup is the product which we create by adding
+soup = bs.BeautifulSoup(source, "lxml")
+
+def result():
+    print(soup.h1)
+    print(soup.div)
